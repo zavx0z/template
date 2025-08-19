e0be0f47d188c2a3a72cb6068aaca7451a30c12a
zavx0z <zavx0z@yahoo.com>
2025-08-19 06:09:41 +0300

    Добавлен новый файл text.t.ts с определениями типов для текстовых узлов: статический текст, динамический текст с одной переменной, вложенные текстовые значения контекста и ядра, а также смешанный текст с шаблоном и несколькими переменными. Это улучшит обработку текстового контента в функции elementsHierarchy.

---
+++ text.t.ts
@@ -0,0 +1,39 @@
+/** Статический текст */
+type TextStatic = {
+  type: "text"
+  value: string
+}
+
+/** Динамический текст с одной переменной */
+type TextDynamicSingle = {
+  type: "text"
+  src: "context" | "core" | "state"
+  key?: string
+}
+
+/** Текст значения контекста, вложенный в массив */
+type TextNestedMapContextSingle = {
+  type: "text"
+  src: ["context", string]
+}
+
+/** Текст значения ядра, вложенный в массив */
+type TextNestedMapCoreSingle = {
+  type: "text"
+  src: ["core", ...string[]]
+  key?: string
+}
+
+/** Смешанный текст с шаблоном и несколькими переменными */
+type TextMixedMulti = {
+  type: "text"
+  template: string
+  items: Array<{ src: "context" | "core" | "state"; key?: string }>
+}
+
+export type NodeText =
+  | TextStatic
+  | TextDynamicSingle
+  | TextNestedMapContextSingle
+  | TextNestedMapCoreSingle
+  | TextMixedMulti
